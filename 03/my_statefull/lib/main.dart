ce85bbb9a37de68668f01aace47880e28e02af7a
Piyush Garg <pgarg2598.ca18@chitkara.edu.in>
2019-11-17 19:09:16 +0530

    Spanish App Completed

--- 03/my_statefull/lib/main.dart
+++ 03/my_statefull/lib/main.dart
@@ -3,14 +3,73 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(MaterialApp(
     home:MyButton(),
+    debugShowCheckedModeBanner: false,
   )
   );
 }
 
 class MyButton extends StatefulWidget{
-
+  @override
+  MyButtonState createState(){
+    return MyButtonState();
+  }
 }
 
 class MyButtonState extends State<MyButton>{
-  
+  int counter = 0;
+  List<String> spanishNumbers = [
+    "Uno",
+    "Dos",
+    "Tres",
+    "Cuatro",
+    "Cinco",
+    "Seis",
+    "Seite",
+    "Ocho",
+    "Nueve",
+    "Dietz"
+  ];
+  String defaultText = "Spanish Numbers";
+
+  void displaySNumbers(){
+    setState(() {
+      defaultText = spanishNumbers[counter];
+      counter++;
+      if(counter==10){
+        counter=0;
+      }
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Stateful app'),
+        backgroundColor: Colors.orange,
+      ),
+      
+      body:Container(child:Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+          Text('[$counter] $defaultText', style: TextStyle(fontSize: 30.0)),
+          Padding(padding: EdgeInsets.all(10.0),),
+          RaisedButton(
+          onPressed: displaySNumbers,
+          child: Text('Call Numbers'),
+          color: Colors.orange,
+          textColor: Colors.white,
+          )
+        ],
+       ),
+      ),
+      )
+    );
+  }
+
+
+
+
 }

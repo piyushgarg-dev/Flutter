216ec998e9e2f989394899c9edec89e263819f22
Piyush Garg <pgarg2598.ca18@chitkara.edu.in>
2019-11-17 15:53:54 +0530

    A great layout 🥰

--- 02/my_stateless/lib/main.dart
+++ 02/my_stateless/lib/main.dart
@@ -5,8 +5,22 @@ void main(){
   var app = MaterialApp(
       title: 'Piyush App',
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+        
+      ),
       home: Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          title: Text('Flutter'),
+          
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){},
+          child: Icon(
+            Icons.add_a_photo,
+          ),
+          
+          ),
       )
   );
 
